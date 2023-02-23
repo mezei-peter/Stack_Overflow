81ba5be1e3ab5d79dfde2c9d27336b7b12715ca1
Máté <mate.lorinczi97@gmail.com>
2023-02-23 14:12:56 +0100

    Add post user endpoint

--- src/main/java/com/codecool/stackoverflowtw/controller/UserController.java
+++ src/main/java/com/codecool/stackoverflowtw/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.codecool.stackoverflowtw.controller;
 
+import com.codecool.stackoverflowtw.controller.dto.NewUserDTO;
 import com.codecool.stackoverflowtw.controller.dto.UserDTO;
+import com.codecool.stackoverflowtw.service.userService.UserAlreadyExistAuthenticationException;
 import com.codecool.stackoverflowtw.service.userService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +25,17 @@ public class UserController {
     @GetMapping("/all")
     public List<UserDTO> listAllUsers() {
        return userService.getAllUsers();
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<String> createUser(@RequestBody NewUserDTO newUserDTO) {
+        try {
+            userService.createNewUser(newUserDTO);
+        } catch (UserAlreadyExistAuthenticationException e) {
+            return new ResponseEntity<>("Username: " + newUserDTO.username() + " already exists",
+                    HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>("User with username: " + newUserDTO.username() + " created.",
+                HttpStatus.CREATED);
     }
 }
