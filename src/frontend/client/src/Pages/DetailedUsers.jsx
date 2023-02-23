45eff0c8181311a78cece4ab9116f0697fdd7fb5
BalintGeri <109275872+BalintGeri@users.noreply.github.com>
2023-02-23 14:06:44 +0100

    implemented detailed users, inst react router dom

--- src/frontend/client/src/Pages/DetailedUsers.jsx
+++ src/frontend/client/src/Pages/DetailedUsers.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import useState from 'react';
+import useEffect from 'react';
 
 const DetailedUsers = () => {
-    return (<div>
+    const [users, setUsers] = useState([]);
 
-    </div>);
+    useEffect(() => {
+        fetch("user/all")
+            .then((response) => response.json())
+            .then((data) => setUsers(data));
+    }, []);
+
+    return (
+        <div>
+            <h2>User List</h2>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
- 
+
 export default DetailedUsers;
