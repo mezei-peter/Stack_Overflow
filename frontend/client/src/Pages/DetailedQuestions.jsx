84677daaefde1bf661ab4fc438d0a2682bea397d
Máté <mate.lorinczi97@gmail.com>
2023-02-23 17:45:40 +0100

    Add detailed question page to frontend

--- frontend/client/src/Pages/DetailedQuestions.jsx
+++ frontend/client/src/Pages/DetailedQuestions.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const fetchQuestion = async(id) => {
+    return await (await fetch(`/questions/${id}`)).json();
+}
 
 const DetailedQuestions = () => {
-    return (<div>
 
+    const {id} = useParams();
+
+    const [question, setQuestion] = useState(null);
+    const [loading, setLodaing] = useState(true);
+
+    useEffect(() => {
+        setLodaing(true);
+
+        const getQuestion = async () => {
+
+            try {
+                const data = await fetchQuestion(id);
+                console.log(data)
+                setQuestion(data);
+                setLodaing(false);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        getQuestion();
+    }, [])
+
+    if(loading) return <div>Loading</div>
+
+    return (<div>
+        <div>
+            Username: {question.username}
+        </div>
+        <div>
+            Created: {question.created}
+        </div>
+        <div>
+            Title: {question.title}
+        </div>
+        <div>
+            Description: {question.description}
+        </div>
+        <div>Answers: </div>
+        <div>
+            {question.answers.map(answer => <div key={answer.answerId}>
+                <div>
+                    {answer.description}
+                </div>
+            </div>)}
+        </div>
     </div>);
 }
  
--- frontend/client/src/index.js
+++ frontend/client/src/index.js
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
         element: <Login/>
       },
       {
-        path: "/question/:id",
+        path: "/questions/:id",
         element: <DetailedQuestions/>
       }, 
       {
